af4970c5454633c33aa8c0c2db5bbf6c8466eb5d
gudrmsglgl <gudrmsglgl@gmail.com>
2025-12-18 17:16:12 +0900

    chore: Tailwind & Shadcn 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import './App.css'
+import "./App.css";
+import { Button } from "./components/ui/button";
+import { cn } from "./lib/utils";
 
 function App() {
-
+  const isActive = false;
 
   return (
-    <div>hello world</div>
-  )
+    <div>
+      <Button>shadcn button</Button>
+      <div
+        className={cn(
+          "w-10 text-[30px]",
+          isActive ? "text-green-500" : "text-red-500",
+        )}
+      >
+        isActive
+      </div>
+      <div className="text-primary">Primary</div>
+      <div className="text-muted">Muted</div>
+      <div className="text-destructive">Destructive</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
