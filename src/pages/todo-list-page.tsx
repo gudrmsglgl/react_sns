6ad36eefb6439f51d7e5c41207b2b0df16d7c847
gudrmsglgl <gudrmsglgl@gmail.com>
2025-12-24 10:47:52 +0900

    feat: TanStack Query 의존성 추가 및  query 추가

--- src/pages/todo-list-page.tsx
+++ src/pages/todo-list-page.tsx
@@ -1,15 +1,19 @@
 import TodoEditor from "@/components/todo-list/todo-editor";
 import TodoItem from "@/components/todo-list/todo-item";
-import { useTodos } from "@/stores/todo";
+
+import { useTodosData } from "@/hooks/queries/use-todos.data";
 
 export default function TodoListPage() {
-  const todos = useTodos();
+  const { data: todos, isLoading, error } = useTodosData();
+
+  if (error) return <div>에러발생: {error.message}</div>;
+  if (isLoading) return <div>로딩 중 입니다....</div>;
 
   return (
     <div className="flex flex-col gap-5 p-5">
       <h1 className="text-2xl font-bold">Todo List Page</h1>
       <TodoEditor />
-      {todos.map((item) => (
+      {todos?.map((item) => (
         <TodoItem key={item.id} {...item} />
       ))}
     </div>
