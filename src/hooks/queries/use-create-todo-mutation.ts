2c108b4e339031448f25611a71b44bea0298a784
gudrmsglgl <gudrmsglgl@gmail.com>
2025-12-28 01:32:46 +0900

    feat: create-todo-mutation 로직 개선
    - 캐시 무효화 대신 캐시 데이터를 업데이트로 변경

--- src/hooks/queries/use-create-todo-mutation.ts
+++ src/hooks/queries/use-create-todo-mutation.ts
@@ -2,6 +2,7 @@ import { useMutation } from "@tanstack/react-query";
 import { createTodo } from "@/api/create-todo";
 import { useQueryClient } from "@tanstack/react-query";
 import { QUERY_KEYS } from "@/lib/constants";
+import type { Todo } from "@/types";
 
 export function useCreateTodoMutation() {
   const queryClient = useQueryClient();
@@ -10,10 +11,14 @@ export function useCreateTodoMutation() {
     mutationFn: createTodo,
     onMutate: () => {},
     onSettled: () => {},
-    onSuccess: () => {
-      //window.location.reload();
-      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.todo.list });
+    onSuccess: (newTodo) => {
+      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+        if (!prevTodos) return [newTodo];
+        return [...prevTodos, newTodo];
+      });
     },
-    onError: () => {},
+    onError: (error) => {
+      window.alert(error.message);
+    },
   });
 }
